2ebe2410dd4575d197f8ac79134763c536c8dc34
Letícia Rezende <leticia.rezende2010@outlook.com>
2025-01-23 10:18:30 -0300

    feat: cria função para chamar endpoint para obter shows

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,13 @@
 import axios from "axios";
+import { Concert } from "../types/Concerts";
 
 const api = axios.create({
   baseURL: process.env.VITE_API_URL,
 });
+
+export async function getConcerts(): Promise<Concert[]> {
+  const concerts = (await api.get("shows")).data;
+  return concerts;
+}
 
 export default api;
