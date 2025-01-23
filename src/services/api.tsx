4e0192d89a40c1479e2daf0abb8af1dafc7d26e0
Letícia Rezende <leticia.rezende2010@outlook.com>
2025-01-23 10:28:29 -0300

    feat: cria função para chamar endpoit para obter palcos

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { Concert } from "../types/Concerts";
+import { Stage } from "../types/Stage";
 
 const api = axios.create({
   baseURL: process.env.VITE_API_URL,
@@ -9,5 +10,8 @@ export async function getConcerts(): Promise<Concert[]> {
   const concerts = (await api.get("shows")).data;
   return concerts;
 }
-
+export async function getStages(): Promise<Stage[]> {
+  const stages = (await api.get("palcos")).data;
+  return stages;
+}
 export default api;
