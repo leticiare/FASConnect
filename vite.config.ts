99022fafb8a8b4718bce294060ac0125aa7d1ff3
Letícia Rezende <leticia.rezende2010@outlook.com>
2025-01-24 09:20:22 -0300

    chore: adiciona tailwind aos plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { config } from "dotenv";
+import tailwindcss from "@tailwindcss/vite";
 config();
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   define: {
     "process.env": process.env,
   },
