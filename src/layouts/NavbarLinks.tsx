2f5b3ea3467edb7e445942a80432335f7406d37e
Letícia Rezende <leticia.rezende2010@outlook.com>
2025-01-24 09:17:35 -0300

    feat: cria componentes com links para navbar

---
+++ src/layouts/NavbarLinks.tsx
@@ -0,0 +1,20 @@
+import { Fab } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import NavLink from "./NavbarLink";
+
+export default function NavbarLinks() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="text-white flex gap-4 items-center">
+        <NavLink to="/">Local</NavLink>
+        <NavLink to="/palcos">Palco</NavLink>
+        <NavLink to="/shows">Shows</NavLink>
+
+        <Fab variant="extended" onClick={() => navigate("/")}>
+          Minha Programação
+        </Fab>
+      </div>
+    </>
+  );
+}
