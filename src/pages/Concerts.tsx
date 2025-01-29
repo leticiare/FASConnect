7a4ed5aa1f7683851c52e639d6df3d45c35a85ea
Letícia Rezende <leticia.rezende2010@outlook.com>
2025-01-28 23:49:49 -0300

    feat: cria função para guardar lista de shows

--- src/pages/Concerts.tsx
+++ src/pages/Concerts.tsx
@@ -1,4 +1,20 @@
+import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Concert as ConcertType } from "../types/Concerts";
+import { getConcerts } from "../services/api";
+import CardWithImage from "../components/CardIWithmage";
+
 export const Concert = () => {
+  const [concerts, setConcerts] = useState<ConcertType[]>([]);
+  const fetchConcerts = async () => {
+    const res = await getConcerts();
+    setConcerts(res);
+  };
+
+  useEffect(() => {
+    fetchConcerts();
+  }, []);
+
   return (
     <>
       <h1>Shows</h1>
