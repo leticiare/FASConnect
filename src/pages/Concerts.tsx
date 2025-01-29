abf1f4e6c279d98b0429a511e242193c1939cb24
Letícia Rezende <leticia.rezende2010@outlook.com>
2025-01-28 23:50:15 -0300

    feat: exibe lista de cards dos shows

--- src/pages/Concerts.tsx
+++ src/pages/Concerts.tsx
@@ -16,8 +16,29 @@ export const Concert = () => {
   }, []);
 
   return (
-    <>
-      <h1>Shows</h1>
-    </>
+    <div className="">
+      <div className="w-4/5 m-auto text-center my-8">
+        <div className="font-secondary">
+          <Typography
+            variant="h3"
+            fontFamily="Delius Unicase"
+            component="div"
+            color="#fff"
+          >
+            Shows
+          </Typography>
+          <p className="text-white text-xl">
+            Abaixo, confira os artistas mais aclamados que vão agitar o FASC
+            2077! Veja as datas, horários e palcos dos shows!
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap gap-4 w-4/5 m-auto justify-between  mb-4">
+        {concerts.map((concert) => (
+          <CardWithImage content={concert} />
+        ))}
+      </div>
+    </div>
   );
 };
