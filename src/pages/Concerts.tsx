92189a11222ca6ba02111eeb28c4f75b90380755
Letícia Rezende <leticia.rezende2010@outlook.com>
2025-01-30 21:20:27 -0300

    feat: cria páginação para exibição do conteúdo

--- src/pages/Concerts.tsx
+++ src/pages/Concerts.tsx
@@ -1,12 +1,28 @@
-import { CircularProgress, Typography } from "@mui/material";
+import {
+  CircularProgress,
+  Pagination,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import { Concert as ConcertType } from "../types/Concerts";
 import { getConcerts } from "../services/api";
 import CardWithImage from "../components/CardIWithmage";
-
+import theme from "../style/theme";
 export const Concert = () => {
   const [concerts, setConcerts] = useState<ConcertType[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const itemsPerPage = 6;
+  const [page, setPage] = useState(1);
+  const handleChange = (_event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+  const startIndex = (page - 1) * itemsPerPage;
+  const displayedConcerts = concerts.slice(
+    startIndex,
+    startIndex + itemsPerPage
+  );
   const fetchConcerts = async () => {
     const res = await getConcerts();
     setConcerts(res);
@@ -40,12 +56,22 @@ export const Concert = () => {
           <CircularProgress color="inherit" />
         </div>
       ) : (
-        <div className="flex flex-wrap gap-4 w-4/5 m-auto justify-around  mb-4">
-          {concerts.map((concert) => (
+        <div className="flex flex-wrap gap-4 w-4/5 m-auto justify-around mb-4">
+          {displayedConcerts.map((concert) => (
             <CardWithImage content={concert} />
           ))}
         </div>
       )}
+      <div className="flex justify-center my-8 ">
+        <ThemeProvider theme={theme}>
+          <Pagination
+            count={Math.ceil(concerts.length / itemsPerPage)}
+            page={page}
+            onChange={handleChange}
+            color="standard"
+          />
+        </ThemeProvider>
+      </div>
     </div>
   );
 };
