593b70ad3368d99022deb52b49cc9a61423bf48c
panyfmc <paulavanessa010@gmail.com>
2025-02-04 17:39:53 -0300

    :sparkles: feat: imagens da landingpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import "./style/index.css";
 import Navbar from "./layouts/Navbar";
+import NSCimg from "../public/NSCimg.png";
+import img2077 from "../public/2077.png";
+
 
 import { AppRoutes } from "./routes/routes";
 
@@ -9,10 +12,25 @@ function App() {
       <header>
         <Navbar />
       </header>
+
       <div>
         <AppRoutes />
       </div>
-      <footer>Todos os direitos reservados.</footer>
+
+      <div className="texto">  
+        <p> 
+            Em 2077, o Festival de Artes de São Cristóvão (FASC) completa 105 anos de celebração da cultura, da arte e da tradição. Ao longo de mais de um século, o FASC se consolidou como um dos maiores e mais importantes encontros artísticos do mundo, unindo gerações, estilos e manifestações culturais de todos os cantos do Brasil.
+            <br /> <br /> Venha celebrar conosco 105 anos de arte e resistência. O FASC continua a ser o coração pulsante da cultura global.
+        </p>
+      </div>
+
+
+      <div className="Img">
+        <img src={NSCimg} alt="NSC" />
+        <img src={img2077} alt="2077" />
+      </div>
+
+
     </>
   );
 }
