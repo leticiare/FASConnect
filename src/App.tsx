0322ed7d6aceb76a69b6ef74dcd0c0e96abf1a40
panyfmc <paulavanessa010@gmail.com>
2025-02-04 22:26:01 -0300

    :recycle: refactor: add texto de apresentacao na landingpage

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,8 @@ function App() {
       <div>
         <AppRoutes />
       </div>
-      <footer>Todos os direitos reservados.</footer>
+      <footer>Em 2077, o Festival de Artes de São Cristóvão (FASC) completa 105 anos de celebração da cultura, da arte e da tradição. Ao longo de mais de um século, o FASC se consolidou como um dos maiores e mais importantes encontros artísticos do mundo, unindo gerações, estilos e manifestações culturais de todos os cantos do Brasil.
+Venha celebrar conosco 105 anos de arte e resistência. O FASC continua a ser o coração pulsante da cultura global.</footer>
     </>
   );
 }
