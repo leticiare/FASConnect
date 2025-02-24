1d85108271b3ce9c1b4aa70e846b7759cd020ead
Letícia Rezende <leticia.rezende2010@outlook.com>
2025-02-24 20:28:41 -0300

    feat: adiciona página de favoritos à navegação

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,11 +2,13 @@ import { useRoutes } from "react-router-dom";
 import { Concert } from "../pages/Concerts";
 import { Stages } from "../pages/Stages";
 import { Mapa } from "../pages/Map";
+import Favorites from "../pages/Favorites";
 
 export const AppRoutes = () => {
   return useRoutes([
     { path: "/shows", element: <Concert /> },
     { path: "/palcos", element: <Stages /> },
-    {path: "/map", element: <Mapa />}
+    { path: "/map", element: <Mapa /> },
+    { path: "/favorites", element: <Favorites /> },
   ]);
 };
