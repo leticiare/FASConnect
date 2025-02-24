76986b3af350c28d06fe03d80c7932e33f66c7b9
Letícia Rezende <leticia.rezende2010@outlook.com>
2025-02-24 08:45:27 -0300

    feat: adiciona botão para favoritar

--- src/components/CardIWithmage.tsx
+++ src/components/CardIWithmage.tsx
@@ -4,15 +4,22 @@ import {
   CardContent,
   Typography,
   ThemeProvider,
+  IconButton,
 } from "@mui/material";
 import { Concert } from "../types/Concerts";
 import darkTheme from "../style/theme";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import DateRangeIcon from "@mui/icons-material/DateRange";
+import { Star, StarBorder } from "@mui/icons-material";
+import useFavorites from "../hooks/useFavorites"; // Hook para favoritar artistas
+
 export interface CardWithImageProps {
   content: Concert;
 }
+
 function CardWithImage({ content }: CardWithImageProps) {
+  const { favorites, toggleFavorite } = useFavorites(); // Hook para gerenciar favoritos
+
   return (
     <ThemeProvider theme={darkTheme}>
       <Card
@@ -23,32 +30,46 @@ function CardWithImage({ content }: CardWithImageProps) {
           bgcolor: "rgba(150, 150, 150, 0.2)",
           backdropFilter: "blur(5px)",
           color: "text.primary",
+          position: "relative",
         }}
       >
+        {/* Imagem */}
         {content?.imgUrl && (
           <CardMedia
             component="img"
             sx={{
-              height: 200, // Altura fixa para a imagem
+              height: 200,
               width: 500,
               objectFit: "cover",
             }}
             image={content.imgUrl}
-            alt="Descrição da imagem"
+            alt={content.name}
           />
         )}
 
         <CardContent>
-          <div className="h-18">
+          <div className="h-18 flex items-center justify-between">
             <Typography variant="h5" component="div">
               {content.name}
             </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {content.description}
-            </Typography>
+
+            {/* Botão de Favoritar */}
+            <IconButton onClick={() => toggleFavorite(content)}>
+              {favorites.some((fav) => fav.name === content.name) ? (
+                <Star color="warning" />
+              ) : (
+                <StarBorder />
+              )}
+            </IconButton>
           </div>
 
-          <div className="flex mt-4 gap-1 ">
+          {/* Descrição */}
+          <Typography variant="body2" color="text.secondary">
+            {content.description}
+          </Typography>
+
+          {/* Localização */}
+          <div className="flex mt-4 gap-1">
             <LocationOnIcon />
             {content.location}
           </div>
