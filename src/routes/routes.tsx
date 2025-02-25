787f74865ba3489ffedd4617703818caa787aeba
Letícia Rezende <leticia.rezende2010@outlook.com>
2025-02-24 23:11:38 -0300

    feat: adiciona a landing page à rota raiz

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,11 +2,13 @@ import { useRoutes } from "react-router-dom";
 import { Concert } from "../pages/Concerts";
 import { Stages } from "../pages/Stages";
 import { Mapa } from "../pages/Map";
+import { LandingPage } from "../pages/LandingPage";
 
 export const AppRoutes = () => {
   return useRoutes([
+    { path: "/", element: <LandingPage /> },
     { path: "/shows", element: <Concert /> },
     { path: "/palcos", element: <Stages /> },
-    {path: "/map", element: <Mapa />}
+    { path: "/map", element: <Mapa /> },
   ]);
 };
