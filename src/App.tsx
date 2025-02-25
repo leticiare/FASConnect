a5bd1fb8aaa5c092b23c4701cf2078a19060c01e
Letícia Rezende <leticia.rezende2010@outlook.com>
2025-02-24 23:40:07 -0300

    fix: adiciona fundo e rodapé

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import "./style/index.css";
-import Navbar from "./layouts/Navbar";
+import Navbar from "./layouts/Navbar/index";
 
 import { AppRoutes } from "./routes/routes";
 
+import Footer from "./layouts/Footer";
+
 function App() {
   return (
-    <>
-      <header>
-        <Navbar />
-      </header>
-
-      <div>
+    <div className="h-screen flex flex-col min-h-screen ">
+      <Navbar />
+      <main className="flex-grow bg-[url(../assets/fundo.jpg)] bg-no-repeat bg-fixed bg-cover bg-center ">
         <AppRoutes />
-      </div>
-    </>
+      </main>
+
+      <Footer />
+    </div>
   );
 }
 
