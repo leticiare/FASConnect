5ebd02a2e623a9eacf3bce881973ded3b8421889
Letícia Rezende <leticia.rezende2010@outlook.com>
2025-02-24 23:40:47 -0300

    feat: adiciona navegação ao nome Fasconnect

--- src/layouts/Navbar/index.tsx
+++ src/layouts/Navbar/index.tsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import NavbarLinks from "./NavbarLinks";
 
 export default function Navbar() {
+  const navigate = useNavigate();
   return (
     <>
       <div className="bg-primary w-full h-16 flex items-center p-2 justify-around">
-        <h1 className="text-white font-primary text-4xl">FASConnect</h1>
+        <h1
+          onClick={() => navigate("/")}
+          className="text-white font-primary text-4xl cursor-pointer"
+        >
+          FASConnect
+        </h1>
         <NavbarLinks />
       </div>
     </>
